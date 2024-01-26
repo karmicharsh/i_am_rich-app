41720569fb6951614cabfc967f43ca05baee2782
karmicharsh <harshworld002@gmail.com>
2024-01-26 20:15:01 +0530

    Intial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I AM RICH'),
+          backgroundColor: Colors.purple,// Set the background color to blue
+          centerTitle: true, // to center the title
+        ),
+      ),
+    ),
+  );
+}
