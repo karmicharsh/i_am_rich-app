2826e968ecd22d918dda1153729a59a54a59139b
karmicharsh <harshworld002@gmail.com>
2024-01-26 21:50:07 +0530

    updated content

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,18 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I AM RICH'),
-          backgroundColor: Colors.purple,// Set the background color to blue
-          centerTitle: true, // to center the title
+          title: Text(
+            'ZORO',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor:
+              Colors.blueGrey[900], // Set the background color to blue
+          centerTitle: true, // to center the title works on true or false
+        ),
+        body: Image(
+          image: NetworkImage('https://imgs.search.brave.com/Sr-c40gL4tN8pwfzYnoS12G4TMd2vRaCjkdo1ePcotM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvem9y/by02MjMteC0xMjgw/LXBpY3R1cmUtY2xh/M2JzM2wwcHF1bGw3/MC5qcGc'),
         ),
       ),
     ),
